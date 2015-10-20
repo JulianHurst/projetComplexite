3ccfe91a06c8fa6313cdb264b151039c004a844f
Julian Hurst <julian.hurst@laposte.net>
2015-10-20 21:21:56 +0200

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,11 +1,5 @@
 #include "struct.h"
 
-//Fonctions pour le graphe matriciel
-/*int arete_m(graphe_m g,sommet x,sommet y);
-void initgraphe_m(graphe_m *gm, int n);
-void printgraph_m(graphe_m g);
-int verification_m(graphe_m G, ens_de_sommets e);*/
-
 //Fonctions pour le graphe par listes d'adjacence
 int arete_l(graphe_l g,sommet x,sommet y);
 int verification_l(graphe_l G, ens_de_sommets e);
