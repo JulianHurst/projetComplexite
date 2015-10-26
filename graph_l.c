0abebc4f3628d7e1ae2d5337ff135fbf45c0625d
Julian Hurst <julian.hurst@laposte.net>
2015-10-26 12:35:04 +0100

    Update graph_l.c

--- graph_l.c
+++ graph_l.c
@@ -51,8 +51,7 @@ int lecture(char * f,graphe_l * g)
     int n, m, i;
     int a, b;
     char chemin[100];
-    strcpy(chemin, "Benchs/");
-    strcat(chemin,f);
+    strcpy(chemin,f);
     fichier = fopen(chemin, "r");
 
     if (fichier != NULL)
